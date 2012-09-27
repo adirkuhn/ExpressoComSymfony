152b69b62da146dbf330b8ebba83359ae9e58900
Cristiano Correa Schmidt <cristiano@yoda.prognus.com.br>
2012-09-27 15:39:13 -0300

    commit

--- src/Expresso/ExpressoBundle/Controller/DefaultController.php
+++ src/Expresso/ExpressoBundle/Controller/DefaultController.php
@@ -3,14 +3,24 @@
 namespace Expresso\ExpressoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use SymfonyRequirements;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function MenuTopAction()
     {
+        $menu = array();
+        $menu[] = array( 'name' => 'Pagina Inicial' , 'icon' => 'MenuTop/agendamento.png' , 'href' => 'Home' );
+        $menu[] = array( 'name' => 'Quadro de Avisos' , 'icon' => 'MenuTop/billboard.png' , 'href' => 'billboard' );
+        $menu[] = array( 'name' => 'Correio' , 'icon' => 'MenuTop/mail.png' , 'href' => 'Mail' );
+        $menu[] = array( 'name' => 'Chamados' , 'icon' => 'MenuTop/serviceCall.png' , 'href' => 'ServiceCall' );
+        $menu[] = array( 'name' => 'Agendamento' , 'icon' => 'MenuTop/scheduling.png' , 'href' => 'Scheduling' );
+        $menu[] = array( 'name' => 'Estatisticas' , 'icon' => 'MenuTop/statistically.png' , 'href' => 'Statistically' );
+        $menu[] = array( 'name' => 'Suporte' , 'icon' => 'MenuTop/support.png' , 'href' => 'Support' );
 
+        $response = new Response(json_encode($menu));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
 }
