bdbf5d158c656fd511b5d80f0a833f175d5b7a09
Gustavo <gupraga@gmail.com>
2012-09-27 16:50:35 -0300

    transformado o Login em um objeto javascript

--- src/WebClient/WebClientBundle/Resources/public/js/login.js
+++ src/WebClient/WebClientBundle/Resources/public/js/login.js
@@ -1,35 +1,39 @@
-var login = {
-    load : function(){
-        API.render("Login", loginObj, 'body');
-        var login = $("body .login-window");
-        login.wijdialog({
-            buttons: {
-                "Acessar": function () {
-                    $(this).parents(".ui-dialog").find("form").submit();
-                    $(this).dialog("close");
-                }
-            },
-            draggable:false,
-            resizable:false,
-            captionButtons:{
-                pin: { visible: false },
-                refresh: { visible: false },
-                toggle: { visible: false },
-                minimize: { visible: false },
-                maximize: { visible: false },
-                close: { visible: false }
-            }
-        });
-        //SOME INTERFACE DETAILS
-        login.find('input[type=button]').button();
-        login.find('input[type=password]').keydown(function(e){
-            if(e.keyCode === 13){
-               $(this).parents(".ui-dialog").find("form").submit();
-            }
-        });
-        login.parents(".ui-dialog").css({"left": "75%", "top": "30%", "border-radius":"5px"});
-    }
+function oLogin(){
 }
-onload = function(){
-    login.load();
+
+oLogin.prototype.load = function(){
+    API.render("Login", loginObj, 'body');
+    var login = $("body .login-window");
+    login.wijdialog({
+        buttons: {
+            "Acessar": function () {
+                $(this).parents(".ui-dialog").find("form").submit();
+                $(this).dialog("close");
+            }
+        },
+        draggable:false,
+        resizable:false,
+        captionButtons:{
+            pin: { visible: false },
+            refresh: { visible: false },
+            toggle: { visible: false },
+            minimize: { visible: false },
+            maximize: { visible: false },
+            close: { visible: false }
+        }
+    });
+    //SOME INTERFACE DETAILS
+    login.find('input[type=button]').button();
+    login.find('input[type=password]').keydown(function(e){
+        if(e.keyCode === 13){
+           $(this).parents(".ui-dialog").find("form").submit();
+        }
+    });
+    login.parents(".ui-dialog").css({"left": "75%", "top": "30%", "border-radius":"5px"});
 }
+
+var Login = new oLogin();
+
+$(document).ready(function(){
+    Login.load();
+});
