537193d59446011b18863384a38e18def1f434cd
Gustavo <gupraga@gmail.com>
2012-09-27 11:52:04 -0300

    Inserção API javascript REST

---
+++ src/WebClient/WebClientBundle/Resources/public/js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var Layout = $("#wrap").layout({
+        applyDefaultStyles: true,
+        north__size:        70,
+        north__closable:    true,
+        north__resizable:   false,
+        north__slidable:    false,       
+        center__maxSize : 0,
+        livePaneResizing: true
+    });
+});
